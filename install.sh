2223d1fe588328bdc4243f349763ad19b697582f
bromag <giuliano.romagnoli@hotmail.com>
2023-04-19 22:39:15 +0200

    adding root or sudo check

--- install.sh
+++ install.sh
@@ -5,5 +5,8 @@
 # tail -f /var/log/tomcat*/*.out 
 # tail -f /var/log/mysql/*.log
 
-
-
+# check if user is root or sudo  
+if ! [ $( id -u ) = 0 ]; then
+    echo "Please run this Script as sudo or root" 1>&2
+    exit1
+fi
