280f1edd9a71c4c265a98ccd4af085b3c551a74f
bromag <giuliano.romagnoli@hotmail.com>
2023-04-19 22:30:31 +0200

    Testing

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 #!/bash/sh
+# check logs by error's 
+# tail -f //log/messages 
+# tail -f /var/log/syslog 
+# tail -f /var/log/tomcat*/*.out 
+# tail -f /var/log/mysql/*.log
 
-apt update && apt upgrade -y
+
+
