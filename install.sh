1b0043ad4c970b5ad1053a939d3cbf203989bbbc
bromag <giuliano.romagnoli@hotmail.com>
2023-04-22 17:46:15 +0200

    guacamole.war and guacamole-server installation

--- install.sh
+++ install.sh
@@ -7,6 +7,25 @@
 # tail -f /var/log/tomcat*/*.out 
 # tail -f /var/log/mysql/*.log
 
+# Colors for output
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+NC='\033[0m' # No Colo
+
+# Version number of Guacamole to install
+# Homepage ~ https://guacamole.apache.org/releases/
+GUACVERSION="1.5.0"
+
+# Log Location
+LOG="/tmp/guacamole_${GUACVERSION}_build.log"
+
+# Ip address from host
+ip=$(hostname -I)
+my_ip=${ip%% *}
+
 # check if user has read the Script
 while true; do
 
@@ -72,3 +91,33 @@ if [ $? !=0 ]; then
     exit 1
 fi
 
+echo "Installing tomcat9 and mariadb-server"
+sleep 2
+apt-get install -y make tomcat9 mariadb-server
+sleep 2
+echo can check if Apache Tomcat is installed correctly: http://$my_ip:8080
+sleep 3
+
+# Set preferred download server from the Apache CDN
+SERVER="http://apache.org/dyn/closer.cgi?action=download&filename=guacamole/${GUACVERSION}"
+echo -e "${BLUE}Downloading files...${NC}"
+
+# Download Guacamole Server
+wget -q --show-progress -O guacamole-server-${GUACVERSION}.tar.gz ${SERVER}/source/guacamole-server-${GUACVERSION}.tar.gz
+if [ $? -ne 0 ];then
+    echo -e "${RED}Failes to download guacamole-server-${GUACVERSION}.tar.gz" 1>&2
+    echo -e "${SERVER}/source/guacamole-server-${GUACVERSION}.tar.gz${NC}"
+    exit 1
+else
+    # Extract Guacmaole Files
+    tar -xzf guacamole-server-${GUACVERSION}.tar.gz
+fi
+echo -e "${GREEN}Downloaded guacamole-server-${GUACVERSION}.tar.gz${NC}"
+
+# Download Guacamole Client
+if [$? -ne 0 ];then
+    echo -e "${RED}Failes to download guacamole-${GUACVERSION}.war" 1>&2
+    echo -e "${SERVER}/binary/guacamole-${GUACVERSION}.war${NC}"
+    exit 1
+fi
+echo -e "${GREEN}Downloaded guacamole-${GUACVERSION}.war${NC}"
