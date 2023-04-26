d786b20869489dc202ee51e3700ef1c84ca14873
bromag <giuliano.romagnoli@hotmail.com>
2023-04-26 23:20:33 +0200

    fix tar

--- guacamol-server-install.sh
+++ guacamol-server-install.sh
@@ -149,9 +149,10 @@ if [ $? -ne 0 ]; then
 else
     # Extract Guacmaole Files
     echo -e "${CYAN}Download complete${NC}"
+    tar -xvzf guacamole-server-${GUACVER}.tar.gz -C /usr/src/
 fi
 
-tar -xvzf guacamole-server-${GUACVER}.tar.gz -C /usr/src/
+
 echo -e "${GREEN}Downloaded guacamole-server-${GUACVERSION}.tar.gz${NC}"
 
 wget -q --show-progress --trust-server-names "https://apache.org/dyn/closer.cgi?action=download&filename=guacamole/$GUACVER/binary/guacamole-$GUACVER.war" -O /usr/src/guacamole-$GUACVER.war
