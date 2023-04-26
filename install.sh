78ae9991494d70d065aedef8585b03de85d42bfe
bromag <giuliano.romagnoli@hotmail.com>
2023-04-26 20:57:05 +0200

    fixing read error

--- install.sh
+++ install.sh
@@ -107,7 +107,7 @@ fi
 
 # check if user has rebootet machine after update and upgrade
 while true; do
-read -p "${YELLOW}If the Script has found update and upgrade please make sure to reboot your system, or the installation will not complete correctly! Do you want to proceed? (y/n) " yn
+read -rp "${YELLOW}If the Script has found update and upgrade please make sure to reboot your system, or the installation will not complete correctly! Do you want to proceed? (y/n) " yn
 case $yn in 
 	[yY] ) echo ok, we will proceed;
 		break;;
